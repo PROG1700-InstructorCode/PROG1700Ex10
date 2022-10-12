b74a69bfa853084294391ed1efc822caf09df838
hamletbon <hamletbon@users.noreply.github.com>
2022-10-12 10:23:20 -0300

    init ex 10 with export and console settings

---
+++ prog1700Ex10Function.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// DO NOT EDIT: The main function to house our program code 
+function main()
+{
+    // enter your code here
+}
+
+//create a function assignHouse 
+
+// DO NOT EDIT: Trigger our main function to launch the program
+if (require.main === module)
+{
+    main();
+}
+
+module.exports.prog1700Ex10 = main
